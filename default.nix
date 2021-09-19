4235b534b3a84a9ebc9e6ac1781a7f9a03e5c0ef
Niels G. W. Serup <ngws@metanohi.name>
2021-09-19 20:56:25 +0200

    Fix Nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with (import <nixpkgs> { });
 let
   # This is the last commit before ghc865 was removed.
   pkgs_ghc865 = import (builtins.fetchTarball {
-    name = "nixpkgs-unstable-ghc844";
+    name = "nixpkgs-unstable-ghc865";
     url = "https://github.com/nixos/nixpkgs/archive/51cfea6e6c4ee557c2398a6815704eb8f39caf46.tar.gz";
     sha256 = "0ivxs6awrvgz2gn5jjhrvyd3lwx74xdjasj6a10zv7hh2rsnp509";
   }) {};
@@ -11,4 +11,5 @@ haskell.lib.buildStackProject {
   name = "reje";
   buildInputs = with pkgs_ghc865; [ pkgconfig SDL SDL_gfx zlib ];
   ghc = pkgs_ghc865.haskell.compiler.ghc865;
+  src = ./.;
 }
